edf7967efb2f019c53e6e322ee8d41f50a84a191
FWMoor <30631816+FWMoor@users.noreply.github.com>
2019-06-27 14:58:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
-fuck this
+#include <stdio.h>
+#include <dirent.h>
+
+int main(void)
+{
+    struct dirent *de;
+    DIR *dr = opendir(".");
+
+    if (dr == NULL)
+    {
+        printf("Could not open current directory" );
+        return 0;
+    }
+    while ((de = readdir(dr)) != NULL)
+        printf("%s\t", de->d_name);
+    printf("\n");
+    closedir(dr);
+    return 0;
+}
