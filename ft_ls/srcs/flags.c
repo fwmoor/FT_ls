e704d3c6c9ee9b373255eac5b4d0a7b5e4b962bc
Frederick Moor <fwmoor@Fredericks-MacBook-Pro.local>
2019-07-17 20:30:49 +0200

    Half working flag check things

--- ft_ls/srcs/flags.c
+++ ft_ls/srcs/flags.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   flags.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fremoor <fremoor@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fwmoor <fwmoor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 11:41:56 by fremoor           #+#    #+#             */
-/*   Updated: 2019/07/17 15:27:57 by fremoor          ###   ########.fr       */
+/*   Updated: 2019/07/17 20:28:37 by fwmoor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,38 +33,34 @@ int				check_flags(char c)
 		return (TIM);
 	else if (c == 'f')
 		return (NOSOR);
-	else if (c == '.')
-		return (DOT);
-	else if (c == '-')
+	else
 		return (256);
-	return (0);
 }
 
 int				get_flags(int ac, char **av)
 {
 	int				i;
+	int				t;
 	int				j;
 	int				flags;
-	int				temp;
 
 	flags = 0;
 	i = 0;
 	while (++i < ac && av[i][0] == '-')
 	{
-		temp = 0;
 		j = 0;
-		if (av[i][j++] == '-')
+		if (av[i][j++] == '-' && av[i][j] != '\0')
 		{
-			while ((av[i][j] == 'l' || av[i][j] == 'a' || av[i][j] == 'R' ||
-			av[i][j] == 'r' || av[i][j] == 't' || av[i][j] == '.' ||
-			av[i][j] == 'f' || av[i][j] == '-') && !(flags & 128))
-				temp |= check_flags(av[i][j++]);
-			if ((temp == 0 || temp & 128))
-				temp |= err_han4(av[i]);
-			if (temp == 0)
-				illegal_flags(av[i][j]);
+			t = j;
+			while (av[i][j])
+				flags |= check_flags(av[i][j++]);
+			if (flags & 256)
+				illegal_flags(av[i][t]);
 		}
-		flags += temp;
+		else if (av[i][j] == '\0')
+			flags |= err_han4(av[i]);
+		else
+			break ;
 	}
 	return (flags);
 }
