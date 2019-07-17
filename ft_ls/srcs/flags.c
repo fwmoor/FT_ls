925f4883e7e8ed6a192b2b4630da310267194149
Frederick Moor <fremoor@c4r20s2.wethinkcode.co.za>
2019-07-17 15:28:49 +0200

    illegal flag checks

--- ft_ls/srcs/flags.c
+++ ft_ls/srcs/flags.c
@@ -6,11 +6,18 @@
 /*   By: fremoor <fremoor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 11:41:56 by fremoor           #+#    #+#             */
-/*   Updated: 2019/07/12 10:20:04 by fremoor          ###   ########.fr       */
+/*   Updated: 2019/07/17 15:27:57 by fremoor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
+
+void			illegal_flags(char flag)
+{
+	ft_printf("ft_ls: illegal option -- %c\n", flag);
+	ft_printf("usage: ft_ls [-Raflrt] [file ...]\n");
+	exit (1);
+}
 
 int				check_flags(char c)
 {
@@ -28,6 +35,8 @@ int				check_flags(char c)
 		return (NOSOR);
 	else if (c == '.')
 		return (DOT);
+	else if (c == '-')
+		return (256);
 	return (0);
 }
 
@@ -36,22 +45,26 @@ int				get_flags(int ac, char **av)
 	int				i;
 	int				j;
 	int				flags;
+	int				temp;
 
 	flags = 0;
-	i = 1;
-	while (i < ac)
+	i = 0;
+	while (++i < ac && av[i][0] == '-')
 	{
+		temp = 0;
 		j = 0;
 		if (av[i][j++] == '-')
 		{
-			while (av[i][j] == 'l' || av[i][j] == 'a' || av[i][j] == 'R' ||
+			while ((av[i][j] == 'l' || av[i][j] == 'a' || av[i][j] == 'R' ||
 			av[i][j] == 'r' || av[i][j] == 't' || av[i][j] == '.' ||
-			av[i][j] == 'f')
-				flags |= check_flags(av[i][j++]);
-			if ((flags == 0 || flags & 128))
-				flags |= err_han4(av[i]);
+			av[i][j] == 'f' || av[i][j] == '-') && !(flags & 128))
+				temp |= check_flags(av[i][j++]);
+			if ((temp == 0 || temp & 128))
+				temp |= err_han4(av[i]);
+			if (temp == 0)
+				illegal_flags(av[i][j]);
 		}
-		i++;
+		flags += temp;
 	}
 	return (flags);
 }
