93f18c32a74b15ccafdc00bb59064476efd663cd
Frederick Moor <fwmoor@Fredericks-MacBook-Pro.local>
2019-07-21 13:31:19 +0200

    teest

--- ft_ls/includes/ft_ls.h
+++ ft_ls/includes/ft_ls.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ls.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fremoor <fremoor@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fwmoor <fwmoor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 09:17:47 by zmahomed          #+#    #+#             */
-/*   Updated: 2019/07/19 14:53:14 by fremoor          ###   ########.fr       */
+/*   Updated: 2019/07/21 13:22:10 by fwmoor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 # include <sys/stat.h>
 # include <stdio.h>
 # include <sys/types.h>
+# include <sys/acl.h>
+# include <sys/xattr.h>
 # include <pwd.h>
 # include <grp.h>
 # include <uuid/uuid.h>
@@ -32,7 +34,10 @@
 # define DOT 32
 # define NOSOR 64
 # define ONE 128
-# define ERROR 256
+# define COLO 256
+# define NORM 512
+# define UGNOM 1024
+# define ERROR 2048
 
 typedef struct		s_dir
 {
@@ -43,6 +48,7 @@ typedef struct		s_dir
 	char			*name;
 	char			*uid;
 	char			*gid;
+	char			*path;
 	off_t			size;
 	time_t			mtime;
 	time_t			ntime;
@@ -57,10 +63,12 @@ int					long_nlink(t_dir *nodes, int flags);
 int					get_flags(int ac, char **av);
 int					ft_printf(const char *format, ...);
 int					err_han(char *path, DIR *dp, int ierrno, int flag);
-char				*convert_un(int uid);
-char				*convert_gn(int gib);
+char				acl_print(t_dir *lst);
+char				*convert_un(int uid, int flags);
+char				*convert_gn(int gib, int flags);
 int					add_args(char **args, int ac, char **av);
-void				display_l(t_dir *lst, char *path);
+void				add_colo(t_dir *lst, int flags);
+void				display_l(t_dir *lst, char *path, int flags);
 void				print_link(t_dir *lst, char *path);
 void				convert_date(char *str);
 void				delete_list(t_dir **list);
@@ -73,10 +81,10 @@ void				recursion(t_dir *list, int flags, char *path);
 void				check_nano(int flags, t_dir *a, t_dir *b, t_dir **result);
 void				fb_split(t_dir *src, t_dir **front, t_dir **back);
 void				basic_print(struct dirent *de, int flags, DIR *dr);
-void				list_add(t_dir **alst, struct dirent *de, char *path);
+void				list_add(t_dir **alst, struct dirent *de, char *path, int f);
 void				recursive_print(struct dirent *de, int flags, char *path);
 t_dir				*s_merge(t_dir *a, t_dir *b, int flags);
 t_dir				*merge_time(t_dir *a, t_dir *b, int flag);
-t_dir				*set_list(struct dirent *de, char *path);
+t_dir				*set_list(struct dirent *de, char *path, int flags);
 
 #endif
